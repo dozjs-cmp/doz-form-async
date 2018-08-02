8ae977e9efd3c87fec333d434f7fd78992d82269
fabioricali <ricalifabio@gmail.com>
2018-08-02 17:12:58 +0200

    remove Doz from bundle :D

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const unminifiedWebpackPlugin = require('unminified-webpack-plugin');
 const WebpackAutoInject = require('webpack-auto-inject-version');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 function dashToCamelCase(str) {
     return str
@@ -11,58 +12,61 @@ function dashToCamelCase(str) {
 
 const libraryName = dashToCamelCase(require('./package').name);
 
-module.exports = {
-    entry: './index.js',
-    output: {
-        filename: './bundle.min.js',
-        library: libraryName,
-        umdNamedDefine: true,
-        libraryTarget: 'umd',
-        globalObject: 'this'
-    },
-    resolve: {
-        modules: ['node_modules'],
-        extensions: ['*', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
+module.exports = (env, argv) => {
+    return {
+        entry: './index.js',
+        output: {
+            filename: './bundle.min.js',
+            library: libraryName,
+            umdNamedDefine: true,
+            libraryTarget: 'umd',
+            globalObject: 'this'
+        },
+        resolve: {
+            modules: ['node_modules'],
+            extensions: ['*', '.js']
+        },
+        externals: argv.mode === 'production' ? [nodeExternals()] : [],
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
                 }
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
+            ]
+        },
+        plugins: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    mangle: true,
+                    comments: false,
+                    compress: {
+                        warnings: false,
+                        drop_console: true
+                    }, include: /\.min\.js$/
+                }
+            }),
+            new WebpackAutoInject({
+                PACKAGE_JSON_PATH: './package.json',
+                SHORT: libraryName,
+                components: {
+                    InjectAsComment: true,
+                    InjectByTag: true
+                },
+                componentsOptions: {
+                    InjectAsComment: {
+                        tag: 'Build version: {version}'
+                    }
+                }
+            }),
+            new unminifiedWebpackPlugin()
         ]
-    },
-    plugins: [
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                mangle: true,
-                comments: false,
-                compress: {
-                    warnings: false,
-                    drop_console: true
-                }, include: /\.min\.js$/
-            }
-        }),
-        new WebpackAutoInject({
-            PACKAGE_JSON_PATH: './package.json',
-            SHORT: libraryName,
-            components: {
-                InjectAsComment: true,
-                InjectByTag: true
-            },
-            componentsOptions: {
-                InjectAsComment: {
-                    tag: 'Build version: {version}'
-                }
-            }
-        }),
-        new unminifiedWebpackPlugin()
-    ]
+    }
 };
